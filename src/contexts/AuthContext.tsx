b7271149d88fec048f6a5b84b3ec7882abe33eb6
Ricardo <ricardoinliner@yahoo.com>
2021-07-16 10:42:04 -0300

    added axios api pointing to backend

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { ReactNode, useContext } from "react";
 import { createContext } from "react";
+import { api } from "../services/api";
 
 type SignInCredentials = {
     email: string;
@@ -22,11 +23,19 @@ export const AuthProvider = ({children}: AuthProviderProps) => {
     const isAuthenticated = false;
     const signIn = async ({email, password} : SignInCredentials) => {
 
-       console.log(email, password)
+        try {
 
+            const response = await api.post("/sessions", {
+                email, 
+                password
+            })
+
+            console.log(response)
+
+        } catch (err){
+            console.log(err)
+        }
     }
-
-
 
     const value = {isAuthenticated, signIn} as AuthContextData;
 
